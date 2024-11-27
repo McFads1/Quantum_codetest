38a7168cb9cc87e19e38e9bb9f93907897bc571e
Fadi Alnabolsi <falnabol@umich.edu>
2024-11-26 19:48:20 -0500

    Feature: DeactivateOrderView

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
@@ -13,3 +16,18 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class DeactivateOrderview(generics.UpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def post(self, request, *args, **kwargs):
+        order = self.get_object()
+        if not order.is_active:
+            return Response({'error': f'Order {order.id} is already deactivated'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        order.is_active = False
+        order.save()
+
+        return Response({'message': f'Order {order.id} has been deactivated'}, status=status.HTTP_200_OK)
