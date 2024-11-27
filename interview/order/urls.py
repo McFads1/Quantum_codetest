02674f1723bc566a61e88a8daa4464093828614c
Fadi Alnabolsi <falnabol@umich.edu>
2024-11-26 20:54:51 -0500

    Challenge 6 - Tags on Orders

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,14 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import (
+    OrderListCreateView,
+    OrderTagListCreateView,
+    OrderTagsForOrderView
+)
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path('<int:order_id>/tags/', OrderTagsForOrderView.as_view(), name='order-tags'),
 ]
